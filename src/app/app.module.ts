b1cbb0dc1b6ac7037f1d479d481b5bc4a9f5839f
Marionel Rivera <jdrr0505@gmail.com>
2018-03-18 18:12:42 -0500

    creado el template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import  { PruebaComponent} from './prueba/prueba.component';
+import { HeaderComponent} from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/prueba/prueba.component.ts
@@ -0,0 +1,11 @@
+import { Component } from  '@angular/core';
+
+@Component({
+  selector: 'prueba',
+  templateUrl: './prueba.component.html'
+})
+
+export class PruebaComponent {
+
+}
+
