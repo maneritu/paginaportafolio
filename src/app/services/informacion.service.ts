45cd9434fe089d07c9b11f51101dd7369cc732b5
Marionel Rivera <jdrr0505@gmail.com>
2018-03-19 14:41:38 -0500

    Servicio desde firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,12 +5,19 @@ import { Http } from '@angular/http';
 export class InformacionService {
   info:any = {};
   cargada:boolean = false;
+  equipo:any[] = [];
+  cargada_nosotros:boolean = false;
 
   constructor(public http:Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
     this.http.get("assets/data/informacion.json")
       .subscribe(
         data => {
-          console.log(data.json());
+          //console.log(data.json());
           this.cargada = true;
           this.info = data.json();
 
@@ -18,4 +25,15 @@ export class InformacionService {
       )
   }
 
+  public carga_sobre_nosotros(){
+    this.http.get("https://egresis-1982.firebaseio.com/equipo.json")
+      .subscribe(
+        data => {
+          console.log(data.json());
+          this.cargada_nosotros = true;
+          this.equipo = data.json();
+
+        }
+      )
+  }
 }
